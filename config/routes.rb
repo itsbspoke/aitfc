7f4bf7ce5a979574ba7ad18b9c83047bdad8b310
bigfleet <jim@jimvanfleet.com>
2013-09-30 20:36:17 -0400

    Made the home page NOT be the sign in page, because devise hates that

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Aitfc::Application.routes.draw do
+  root to: "events#index"
+
   resources :events
 
-  devise_scope :user do; root to: 'devise/sessions#new'; end
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
