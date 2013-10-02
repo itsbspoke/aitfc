7ee3b64c8259da801d6b8e29349f44e56f0c96c0
bigfleet <jim@jimvanfleet.com>
2013-10-02 16:42:58 -0400

    Moving login spec to new location

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe "Creating an account" do
+
+  context "in error cases" do
+
+
+  end
+
+  context "when successful" do
+
+    it "should run something" do
+      visit root_path
+      save_and_open_page
+    end
+
+  end
+end
+
+
+describe "Logging in" do
+
+  context "in error cases" do
+
+  end
+
+  context "when successful" do
+
+  end
+
+end
+
