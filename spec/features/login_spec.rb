2b27a682917aa0592aeeaeb97a3db8e375060207
bigfleet <jim@jimvanfleet.com>
2013-10-02 18:52:31 -0400

    Added error test for signup

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -4,6 +4,19 @@ describe "Creating an account" do
 
   context "in error cases" do
 
+    it "should not create a new user" do
+      visit root_path
+      click_link "Sign Up"
+      lambda{
+        within("#new_user") do
+          #fill_in "Email", :with => Forgery(:internet).email_address
+          fill_in "Password", :with => "12345678"
+          fill_in "Password confirmation", :with => "12345678"
+          click_button "Sign up"
+        end
+        expect(page).to have_content("error")
+      }.should_not change(User, :count)
+    end
 
   end
 
