6dc771012a3587a3d88141fdb7906699e6799e79
bigfleet <jim@jimvanfleet.com>
2013-10-02 17:02:57 -0400

    A passing case

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -9,9 +9,18 @@ describe "Creating an account" do
 
   context "when successful" do
 
-    it "should run something" do
+    it "should create a new user" do
       visit root_path
-      save_and_open_page
+      click_link "Sign Up"
+      lambda{
+        within("#new_user") do
+          fill_in "Email", :with => Forgery(:internet).email_address
+          fill_in "Password", :with => "12345678"
+          fill_in "Password confirmation", :with => "12345678"
+          click_button "Sign up"
+        end
+        expect(page).to have_content "Welcome"
+      }.should change(User, :count).by(1)
     end
 
   end
