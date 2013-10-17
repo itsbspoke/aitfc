80b4354883a1aaff31562b7b3c6e0b2d7d5bbf29
bigfleet <jim@jimvanfleet.com>
2013-10-16 20:49:48 -0400

    Adding a validation to require a story body

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Story do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "validation" do
+    subject { build(:story) }
+    it { should be_valid }
+    it "should require a body" do
+      build(:story, body: nil).should_not be_valid
+    end
+  end
 end
