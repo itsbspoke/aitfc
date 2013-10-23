14b385dcbb456d087d8f6d9b071329c16ad65d1c
bigfleet <jim@jimvanfleet.com>
2013-10-23 19:15:00 -0400

    I hate you test directory GET OUT OF MY LIFE

--- test/factories/events.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :event do
-    start_time 2.weeks.from_now
-    venue "MyString"
-    sequence(:title) {|n| "All In To Fight Cancer #{n}" }
-  end
-end
